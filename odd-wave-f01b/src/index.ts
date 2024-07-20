72b5955b7018e333eec6ce288056e71e83c40709
Paul Murray <44061707+Code4Wyatt@users.noreply.github.com>
2024-07-20 22:26:49 +0100

    Set up CloudFlare AI workers and translate document

--- odd-wave-f01b/src/index.ts
+++ odd-wave-f01b/src/index.ts
@@ -1,18 +1,10 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import OpenAI from 'openai';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+interface Bindings {
+	OPEN_AI_KEY: string;
+	AI: Ai;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
