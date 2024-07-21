89a088ea50a4f55200e6d94b698de302f9b61de2
Paul Murray <44061707+Code4Wyatt@users.noreply.github.com>
2024-07-21 13:15:12 +0100

    Add ability to translate and chat to documents

--- odd-wave-f01b/src/index.ts
+++ odd-wave-f01b/src/index.ts
@@ -1,4 +1,4 @@
-import OpenAI from 'openai';
+import OpenAI from 'openai'
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 
@@ -8,3 +8,67 @@ interface Bindings {
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+	'/*',
+	cors({
+		origin: '*',
+		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
+		allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT'],
+		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	})
+);
+
+app.post('/translateDocument', async (c) => {
+	const { documentData, targetLang } = await c.req.json();
+
+	// Generate a summary of the document
+	const summaryResponse = await c.env.AI.run('@cf/facebook/bart-large-cnn', {
+		input_text: documentData,
+		max_length: 1000,
+	})
+
+	// Translate the summary into selected language
+	const response = await c.env.AI.run('@cf/meta/m2m100-1.2b', {
+		text: summaryResponse.summary,
+		source_lang: 'english',
+		target_lang: targetLang,
+	})
+
+	console.log(response)
+
+	return new Response(JSON.stringify(response))
+});
+
+app.post('/chatToDocument', async (c) => {
+	const openai = new OpenAI({
+		apiKey: c.env.OPEN_AI_KEY,
+	})
+
+	const { documentData, question } = await c.req.json()
+
+	const chatCompletion = await openai.chat.completions.create({
+		messages: [
+			{
+				role: 'system',
+				content:
+					'You are an assistant helping the user to chat to a document, I am providing a JSON file of the markdown for the document. Using this, answer the users question in the clearst way possible, the document is about ' +
+					documentData,
+			},
+			{
+				role: 'user',
+				content: 'My question is: ' + question
+			},
+		],
+		model: 'gpt-4o-mini-2024-07-18',
+		temperature: 0.5,
+	})
+
+	const response = chatCompletion.choices[0].message.content;
+
+	return c.json({ message: response })
+})
+
+export default app;
